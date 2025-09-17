7236d4559e394835e73ea952b0738f0bad14bb5f
roffy58 <shubhmsagu563sivam@gmail.com>
2025-09-17 18:38:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-
+import path from "path":
+import react from "@vitejs/plugin-react":
 export default defineConfig({
   plugins: [
     react(),
